44cd1cf0dfefdabec9b2dd034ad6ab2a8228a3cc
HAR00N <rustic606@gmail.com>
2022-05-01 19:42:25 +0900

    22.05.01 setting 진행중

--- src/main.js
+++ src/main.js
@@ -13,8 +13,8 @@
 
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from '../src/routers/router'
+import store from './store/store'
 import './plugins/base'
 import './plugins/chartist'
 import './plugins/vee-validate'
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,23 @@
 module.exports = {
   devServer: {
     disableHostCheck: true,
+    overlay: false, 
+    port: 4000, 
+    contentBase: '.',
+    proxy: {
+      '^/api/': {
+        target: 'http://localhost:8080', // 요청할 서버 주소
+        pathRewrite: { '^/api': '' },
+        changeOrigin: true,
+        logLevel: 'debug', // 터미널에 proxy 로그가 찍힌다. 
+      }
+    }
+  },
+  
+  lintOnSave: true,
+  css: {
+    // Enable CSS source maps.
+    sourceMap: process.env.NODE_ENV !== "production",
   },
 
   transpileDependencies: ['vuetify'],
