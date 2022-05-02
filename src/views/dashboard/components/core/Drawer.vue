bae319b12e57e58b853ad94ff02d029e2a53ee87
khlee <rustic606@gmail.com>
2022-05-02 17:10:39 +0900

    22.05.02 update

--- src/views/dashboard/components/core/Drawer.vue
+++ src/views/dashboard/components/core/Drawer.vue
@@ -1,51 +1,57 @@
 <template>
-  <v-navigation-drawer
-    id="core-navigation-drawer"
-    v-model="drawer"
-    :dark="barColor !== 'rgba(228, 226, 226, 1), rgba(255, 255, 255, 0.7)'"
-    :expand-on-hover="expandOnHover"
-    :right="$vuetify.rtl"
-    :src="barImage"
-    mobile-break-point="960"
-    app
-    width="260"
-    v-bind="$attrs"
-  >
-    <template v-slot:img="props">
-      <v-img :gradient="`to bottom, ${barColor}`" v-bind="props" />
-    </template>
+	<v-navigation-drawer
+		id="core-navigation-drawer"
+		v-model="drawer"
+		:dark="barColor !== 'rgba(228, 226, 226, 1), rgba(255, 255, 255, 0.7)'"
+		:expand-on-hover="expandOnHover"
+		:right="$vuetify.rtl"
+		:src="barImage"
+		mobile-break-point="960"
+		app
+		width="260"
+		v-bind="$attrs"
+	>
+		<template v-slot:img="props">
+			<v-img :gradient="`to bottom, ${barColor}`" v-bind="props" />
+		</template>
 
-    <v-divider class="mb-1" />
+		<v-divider class="mb-1" />
 
-    <v-list-item class="pa-0">
-      <v-list-item-content class="pa-0">
-        <v-list-item-title class="title d-flex align-center mb-0">
-          <div class="v-navigation-drawer-brand pa-5 d-flex align-center">
-            <v-img
-              src="src/assets/logo.png"
-              class="navbar-brand-img ms-3"
-              width="40"
-            >
-            </v-img>
-            <span 
-              class="ms-2 font-weight-bold text-lg"
-            >HAR00N's page</span>
-          </div>
-        </v-list-item-title>
-      </v-list-item-content>
-    </v-list-item>
 
-    <!--
+    <!-- https://vuetifyjs.com/en/styles/spacing/ 참고해서 여백 주기 -->
+    <v-list-item class="ms-3 mb-2 mt-4 d-flex align-center">
+			<v-list-item-tile>
+				<v-img src="src/assets/logo.png" width="35"> </v-img>
+			</v-list-item-tile>
+
+			<v-list-item-title title class="display-2 ms-3"
+				>Haroon's Page</v-list-item-title
+			>
+		</v-list-item>
+
+		<!-- 부동산원 코드 -->
+		<!-- 
+		<v-list-item class="pa-0">
+			<v-list-item-content class="pa-0">
+				<v-list-item-title class="title d-flex align-center mb-0">
+					<div class="v-navigation-drawer-brand pa-5 d-flex align-center">
+						<v-img
+							src="src/assets/logo.png"
+							class="navbar-brand-img ms-3"
+							width="40"
+						>
+						</v-img>
+						<span class="ms-2 font-weight-bold text-lg">HAR00N's Page</span>
+					</div>
+				</v-list-item-title>
+			</v-list-item-content>
+		</v-list-item> -->
+
+		<!-- 초기세팅 코드 -->
+		<!-- 
     <v-list dense nav>
       <v-list-item>
-        <v-row>
-          <v-col>
-            <v-img src="src/assets/logo.png" class="my-3" contain height="50" />
-          </v-col>
-
-          <v-col> Haroon's Page </v-col>
-            </v-row>
-        
+           
         <v-list-item-avatar
           class="align-self-center"
           color="white"
@@ -61,128 +67,131 @@
           <v-list-item-title
             class="display-1"
             v-text="profile.title"
-          />
+          />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
         </v-list-item-content>
         
       </v-list-item>
     </v-list>
--->
-    <v-divider class="mb-2" />
+  -->
 
-    <v-list expand nav>
-      <!-- Style cascading bug  -->
-      <!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
-      <div />
+		<v-divider class="mb-2" />
 
-      <template v-for="(item, i) in computedItems">
-        <base-item-group v-if="item.children" :key="`group-${i}`" :item="item">
-          <!--  -->
-        </base-item-group>
+		<v-list expand nav>
+			<!-- Style cascading bug  -->
+			<!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
+			<div />
 
-        <base-item v-else :key="`item-${i}`" :item="item" />
-      </template>
+			<template v-for="(item, i) in computedItems">
+				<base-item-group v-if="item.children" :key="`group-${i}`" :item="item">
+					<!--  -->
+				</base-item-group>
 
-      <!-- Style cascading bug  -->
-      <!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
-      <div />
-    </v-list>
+				<base-item v-else :key="`item-${i}`" :item="item" />
+			</template>
 
-    <template v-slot:append>
-      <base-item
-        :item="{
-          title: $t('upgrade'),
-          icon: 'mdi-package-up',
-          to: '/upgrade',
-        }"
-      />
-    </template>
-  </v-navigation-drawer>
+			<!-- Style cascading bug  -->
+			<!-- https://github.com/vuetifyjs/vuetify/pull/8574 -->
+			<div />
+		</v-list>
+
+		<!--
+		<template v-slot:append>
+			<base-item
+				:item="{
+					title: $t('upgrade'),
+					icon: 'mdi-package-up',
+					to: '/upgrade',
+				}"
+			/>
+		</template>
+    -->
+	</v-navigation-drawer>
 </template>
 
 <script>
 // Utilities
-import { mapState } from "vuex";
+import { mapState } from 'vuex';
 
 export default {
-  name: "DashboardCoreDrawer",
+	name: 'DashboardCoreDrawer',
 
-  props: {
-    expandOnHover: {
-      type: Boolean,
-      default: false,
-    },
-  },
+	props: {
+		expandOnHover: {
+			type: Boolean,
+			default: false,
+		},
+	},
 
-  data: () => ({
-    items: [
-      {
-        icon: "mdi-view-dashboard",
-        title: "dashboard",
-        to: "/",
-      },
-      {
-        icon: "mdi-account",
-        title: "user",
-        to: "/pages/user",
-      },
-      {
-        title: "rtables",
-        icon: "mdi-clipboard-outline",
-        to: "/tables/regular-tables",
-      },
-      {
-        title: "typography",
-        icon: "mdi-format-font",
-        to: "/components/typography",
-      },
-      {
-        title: "icons",
-        icon: "mdi-chart-bubble",
-        to: "/components/icons",
-      },
-      {
-        title: "google",
-        icon: "mdi-map-marker",
-        to: "/maps/google-maps",
-      },
-      {
-        title: "notifications",
-        icon: "mdi-bell",
-        to: "/components/notifications",
-      },
-    ],
-  }),
+	data: () => ({
+		items: [
+			{
+				icon: 'mdi-view-dashboard',
+				title: 'dashboard',
+				to: '/',
+			},
+			{
+				icon: 'mdi-account',
+				title: 'user',
+				to: '/pages/user',
+			},
+			{
+				title: 'rtables',
+				icon: 'mdi-clipboard-outline',
+				to: '/tables/regular-tables',
+			},
+			{
+				title: 'typography',
+				icon: 'mdi-format-font',
+				to: '/components/typography',
+			},
+			{
+				title: 'icons',
+				icon: 'mdi-chart-bubble',
+				to: '/components/icons',
+			},
+			{
+				title: 'google',
+				icon: 'mdi-map-marker',
+				to: '/maps/google-maps',
+			},
+			{
+				title: 'notifications',
+				icon: 'mdi-bell',
+				to: '/components/notifications',
+			},
+		],
+	}),
 
-  computed: {
-    ...mapState(["barColor", "barImage"]),
-    drawer: {
-      get() {
-        return this.$store.state.drawer;
-      },
-      set(val) {
-        this.$store.commit("SET_DRAWER", val);
-      },
-    },
-    computedItems() {
-      return this.items.map(this.mapItem);
-    },
-    profile() {
-      return {
-        avatar: true,
-        title: this.$t("avatar"),
-      };
-    },
-  },
+	computed: {
+		...mapState(['barColor', 'barImage']),
+		drawer: {
+			get() {
+				return this.$store.state.drawer;
+			},
+			set(val) {
+				this.$store.commit('SET_DRAWER', val);
+			},
+		},
+		computedItems() {
+			return this.items.map(this.mapItem);
+		},
+		profile() {
+			return {
+				avatar: true,
+				title: this.$t('avatar'),
+			};
+		},
+	},
 
-  methods: {
-    mapItem(item) {
-      return {
-        ...item,
-        children: item.children ? item.children.map(this.mapItem) : undefined,
-        title: this.$t(item.title),
-      };
-    },
-  },
+	methods: {
+		mapItem(item) {
+			return {
+				...item,
+				children: item.children ? item.children.map(this.mapItem) : undefined,
+				title: this.$t(item.title),
+			};
+		},
+	},
 };
 </script>
 
